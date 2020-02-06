ebb4ac2e021cfa44766d07d4a0ef2c382b5eef38
theborakompanioni <theborakompanioni+github@gmail.com>
2020-02-06 17:04:08 +0100

    pwa: add manifest.json and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import {environment} from '../environments/environment';
 @NgModule({
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
+    ServiceWorkerModule.register('./ngsw-worker.js', {enabled: environment.production}),
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
